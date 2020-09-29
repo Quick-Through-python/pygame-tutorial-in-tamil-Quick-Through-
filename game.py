3a80c7a7ee24f3bc5185ff4c77b90329112dce77
Quick Through <71961217+Quick-Through-python@users.noreply.github.com>
2020-09-29 13:34:08 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,30 @@
+import pygame as pg
+
+pg.init()
+win=pg.display.set_mode((500,500))
+pg.display.set_caption("bigoo")
+
+x,y=50,50
+width,height=40,60
+vel=5
+
+run=True
+while run:
+    pg.time.delay(100)
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            run=False
+    keys=pg.key.get_pressed()
+    if keys[pg.K_LEFT]:
+        x-=vel
+    if keys[pg.K_RIGHT]:
+        x+=vel
+    if keys[pg.K_UP]:
+        y-=vel
+    if keys[pg.K_DOWN]:
+        y+=vel
+    win.fill((0,0,0))
+    pg.draw.rect(win,(255,0,0),(x,y,width,height))
+    pg.display.update()
+    
+pg.quit()
